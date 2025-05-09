96f3154cff071c5fcec94cfe2462e858f431be2c
Bubu <b11117029@gemail.yuntech.edu.tw>
2025-05-09 16:04:11 +0800

    feat: add task id page

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,48 @@
 "use client";
 import TaskList from "@/components/TaskList";
-import Image from "next/image";
-import { useState } from "react";
+import next from "next";
+import { loadComponents } from "next/dist/server/load-components";
+import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
 
+  const [nextId, setNextId] =useState(1);
+  
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    setTasks(savedTasks);
+    const maxId = savedTasks.reduce((max, task) => Math.max(max, task.id), 0);
+    setNextId(maxId +1);
+  }, []);
+  
   const addTask = () => {
     console.log("Before" + tasks);
-    const updatedTasks = [ ...tasks, newTask];
+    console.log("NewTask:" + newTask);
+    const newTaskObj = {
+      id: nextId,
+      title: newTask,
+      description: '',
+    };
+    const updatedTasks = [ ...tasks, newTaskObj];
     setTasks(updatedTasks);
     console.log("After" + updatedTasks);
-  };
+    setNewTask('');
 
+    setNextId(nextId +1)
+    localStorage.setItem('task',JSON.stringify(updatedTasks));
+
+};
+
+const handleDelete = (index) =>{
+  const newTask = tasks.filter((_, i) => i !== index);
+  setTasks(newTaskObj);
+  localStorage.setItem('tasks', JSON.stringify(newTaskObj));
+}
   return (
-    <main className="p-4">
+    <main className="p-4 max-w-md mx-auto">
       <h1 className="text-2xl font-bold mb-4">Task Board</h1>
       <div className="flex gap-2 mb-4">
         <input
